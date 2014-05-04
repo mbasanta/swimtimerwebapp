b83152d6c19442a4c4977f9326585d38c936e201
Matthew Basanta <matthew.basanta@gmail.com>
2014-05-04 16:43:19 -0400

    Initial commit

---
+++ webapp/webapp/settings/test.py
@@ -0,0 +1,21 @@
+"""
+This is an example settings/test.py file.
+Use this settings file when running tests.
+These settings overrides what's in settings/base.py
+"""
+
+from .base import *
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
+}
+
+SECRET_KEY = '*+!6lx62nme7a)qb7=-fp+i31j1hklfajwpcv7utl9)r#@4hu6'
