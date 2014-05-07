900dcf60359af0ad24a29b4d8f7f30203f8884e6
Matthew Basanta <matthew.basanta@gmail.com>
2014-05-06 21:21:08 -0400

    Update to serve media files through dj-static

--- webapp/webapp/settings/base.py
+++ webapp/webapp/settings/base.py
@@ -82,7 +82,9 @@ SESSION_COOKIE_SECURE = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.example.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+#MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+# Modified for dj-static
+MEDIA_ROOT = 'media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -93,7 +95,9 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.example.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# Modified for dj-static
+STATIC_ROOT = 'staticfiles'
 
 # URL prefix for static files.
 # Example: "http://media.example.com/static/"
