290c74ba19ab9d837ca26f01eab14fb32f0eaedc
Matthew Basanta <matthew.basanta@gmail.com>
2014-05-06 20:33:17 -0400

    Settings needed for gunicorn to server static files.

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -2,6 +2,7 @@
 
 from django.conf import settings
 from django.conf.urls import include, patterns, url
+from django.conf.urls.static import static
 from django.contrib import admin
 admin.autodiscover()
 
@@ -20,6 +21,9 @@ urlpatterns = patterns('',
     url(r'^bad/$', bad),
     url(r'', include('base.urls')),
 )
+
+urlpatterns += static(settings.STATIC_URL,
+                      document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
