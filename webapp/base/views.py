d6bd0238f063bf7aa3aa0bb0df6ec8f2df3b1fbb
Matthew Basanta <matthew.basanta@gmail.com>
2014-06-10 19:24:26 -0400

    Create custom user model

--- webapp/base/views.py
+++ webapp/base/views.py
@@ -1,8 +1,11 @@
 """ Views for the base application """
 
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
 
 def home(request):
     """ Default view for the root """
     return render(request, 'base/home.html')
+
+User = get_user_model()
