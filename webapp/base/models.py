aa7a1407c3dbb59f82e01f2b962592209cd137a0
Matthew Basanta <matthew.basanta@gmail.com>
2014-06-11 20:39:21 -0400

    Initial working copy of custom user class, changed primary user id to email address, resolves issue #7

--- webapp/base/models.py
+++ webapp/base/models.py
@@ -4,26 +4,37 @@ from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
+from datetime import date
 
 
 class AppUserManager(BaseUserManager):
     '''Custom user manager to use email as login'''
-    def create_user(self, email, password=None):
-        '''Create user'''
+    def create_user(self, email, date_of_birth, password=None):
+        '''
+        Create and save user with the given email, date of birth,
+        and password
+        '''
         if not email:
             raise ValueError('Users must have an email address')
 
         user = self.model(
-            email=AppUserManager.normalize_email(email)
+            email=AppUserManager.normalize_email(email),
+            date_of_birth=date_of_birth
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password):
-        '''Create superuser'''
-        user = self.create_user(email, password=password)
+    def create_superuser(self, email, date_of_birth, password):
+        '''
+        Create superuser with the given email, date of birth and
+        password
+        '''
+        user = self.create_user(email,
+            password=password,
+            date_of_birth=date_of_birth
+        )
         user.is_admin = True
         user.save(using=self._db)
         return user
@@ -34,16 +45,21 @@ class AppUser(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=30, blank=True)
     email = models.EmailField(_('email address'), max_length=254, unique=True)
+    date_of_birth = models.DateField(_('date of birth'),
+        default=date(1900, 1, 1))
     is_staff = models.BooleanField(_('staff status'), default=False,
         help_text=_('Designates whether the user can log into this admin '
                     'site.'))
     is_active = models.BooleanField(_('active'), default=True,
         help_text=_('Designates whether this user should be treated as '
                     'active. Unselect this instead of deleting accounts.'))
+    is_admin = models.BooleanField(_('admin status'), default=False,
+        help_text=_('Designates whether the user is an admin on the '
+                    'site.'))
     date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
 
     USERNAME_FIELD = 'email'
-    #REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['date_of_birth']
 
     objects = AppUserManager()
 
@@ -51,7 +67,7 @@ class AppUser(AbstractBaseUser, PermissionsMixin):
         return self.first_name + " " + self.last_name
 
     def get_short_name(self):
-        return self.email
+        return self.first_name
 
     def __unicode__(self):
         return self.email
