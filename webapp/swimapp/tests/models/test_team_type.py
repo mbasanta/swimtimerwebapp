8e686350e78affde5b7982b2a32af841e2fd2fc2
Matthew Basanta <matthew.basanta@gmail.com>
2014-06-26 21:38:47 -0400

    update pylint errors

--- webapp/swimapp/tests/models/test_team_type.py
+++ webapp/swimapp/tests/models/test_team_type.py
@@ -3,10 +3,11 @@ from django.test import TestCase
 from swimapp.models.team_type import TeamType
 
 
-class TeamTypeTest(TestCase):
+class TeamTypeTest(TestCase):  # pylint: disable=R0904
     '''Test TeamType Class'''
 
     def test_unicode_method(self):
         '''Ensure unicode method returns string of type_name'''
+        # pylint: disable=E1123, E1120
         team_type = TeamType(type_abbr='NA', type_name='Name')
         self.assertEqual(str(team_type), team_type.type_name)
