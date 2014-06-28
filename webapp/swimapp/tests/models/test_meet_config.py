e0c6e5f1ff929a65ce174400726dbf37028029d9
Matthew Basanta <matthew.basanta@gmail.com>
2014-06-27 20:15:14 -0400

    Update testing of type classes

--- webapp/swimapp/tests/models/test_meet_config.py
+++ webapp/swimapp/tests/models/test_meet_config.py
@@ -1,4 +1,5 @@
-'''tests for team_config'''
+'''tests for meet_config'''
+# pylint: disable=E1101
 from django.test import TestCase
 from swimapp.models.meet_config import MeetConfig
 
@@ -6,8 +7,20 @@ from swimapp.models.meet_config import MeetConfig
 class MeetConfigTest(TestCase):  # pylint: disable=R0904
     '''Test MeetConfig Class'''
 
+    def setUp(self):
+        '''Setup tests'''
+        MeetConfig.objects.create(type_name='One')
+        MeetConfig.objects.create(type_name='Two')
+
+    def test_objects_are_saved(self):
+        '''Test objects are saved to the db'''
+        configs = MeetConfig.objects.all()
+
+        self.assertEqual(configs.count(), 2)
+        self.assertEqual(configs[0].type_name, 'One')
+        self.assertEqual(configs[1].type_name, 'Two')
+
     def test_unicode_method(self):
         '''Ensure unicode method returns string of type_name'''
-        # pylint: disable=E1123, E1120
-        meet_confgi = MeetConfig(type_name='Name')
-        self.assertEqual(str(meet_confgi), meet_confgi.type_name)
+        config = MeetConfig.objects.first()
+        self.assertEqual(str(config), config.type_name)
--- webapp/swimapp/tests/models/test_team_registration.py
+++ webapp/swimapp/tests/models/test_team_registration.py
@@ -1,4 +1,5 @@
 '''tests for team_registration'''
+# pylint: disable=E1101
 from django.test import TestCase
 from swimapp.models.team_registration import TeamRegistration
 
@@ -6,8 +7,20 @@ from swimapp.models.team_registration import TeamRegistration
 class TeamRegistrationTest(TestCase):  # pylint: disable=R0904
     '''Test TeamRegistration Class'''
 
+    def setUp(self):
+        '''Setup tests'''
+        TeamRegistration.objects.create(type_abbr='1', type_name='One')
+        TeamRegistration.objects.create(type_abbr='2', type_name='Two')
+
+    def test_objects_are_saved(self):
+        '''Test objects are saved to the db'''
+        registrations = TeamRegistration.objects.all()
+
+        self.assertEqual(registrations.count(), 2)
+        self.assertEqual(registrations[0].type_name, 'One')
+        self.assertEqual(registrations[1].type_name, 'Two')
+
     def test_unicode_method(self):
         '''Ensure unicode method returns string of type_name'''
-        # pylint: disable=E1123, E1120
-        registration = TeamRegistration(type_abbr='NA', type_name='Name')
+        registration = TeamRegistration.objects.first()
         self.assertEqual(str(registration), registration.type_name)
