5051ab1cf380d6cc116aee6de6e18b61d3c2f37f
Matthew Basanta <matthew.basanta@gmail.com>
2014-06-28 14:32:53 -0400

    Add test for teams

---
+++ webapp/swimapp/tests/models/test_team.py
@@ -0,0 +1,50 @@
+'''tests for api teambyuser'''
+# pylint: disable=E1101
+from django.test import TestCase
+from base.models import AppUser
+from swimapp.models.team import Team, TeamType, TeamRegistration
+
+
+class TeamsByUserTest(TestCase):  # pylint: disable=R0904
+    '''Test teamsbyuser api call'''
+
+    def setUp(self):
+        '''Setup tests'''
+        user = AppUser.objects.create(email='test@example.com')
+        reg = TeamRegistration.objects.create(type_abbr='1', type_name='One')
+        the_type = TeamType.objects.create(type_abbr='2', type_name='Two')
+        team = Team.objects.create(
+            team_abbr='abbr',
+            team_name='team_name',
+            team_short_name='team_short_name',
+            team_type=the_type,
+            team_color1='#111111',
+            team_color2='#222222',
+            addr_name='addr_name',
+            addr='addr',
+            addr_city='addr_city',
+            addr_state='KY',
+            addr_zip='40514',
+            addr_country='USA',
+            latitude=38.123,
+            longitude=-84.123,
+            team_reg=reg,
+            daytime_phone='123-456-7890',
+            evening_phone='987-123-4567',
+            fax='567-1234-0987',
+            email='none@example.com',
+        )
+        team.users_set = [user]
+        team.save()
+
+    def test_objects_are_saved(self):
+        '''Test objects are saved to the db'''
+        teams = Team.objects.all()
+
+        self.assertEqual(teams.count(), 1)
+        self.assertEqual(teams[0].team_name, 'team_name')
+
+    def test_unicode_method(self):
+        '''Ensure unicode method returns string of team_short_name'''
+        team = Team.objects.first()
+        self.assertEqual(str(team), team.team_short_name)
