b85031652b48f7d159c33be556f19ae2c699c03d
Matthew Basanta <matthew.basanta@gmail.com>
2014-06-27 20:15:14 -0400

    Update testing of type classes

--- webapp/swimapp/tests/models/test_team_type.py
+++ webapp/swimapp/tests/models/test_team_type.py
@@ -1,4 +1,5 @@
 '''tests for team_type'''
+# pylint: disable=E1101
 from django.test import TestCase
 from swimapp.models.team_type import TeamType
 
@@ -6,8 +7,20 @@ from swimapp.models.team_type import TeamType
 class TeamTypeTest(TestCase):  # pylint: disable=R0904
     '''Test TeamType Class'''
 
+    def setUp(self):
+        '''Setup tests'''
+        TeamType.objects.create(type_abbr='1', type_name='One')
+        TeamType.objects.create(type_abbr='2', type_name='Two')
+
+    def test_objects_are_saved(self):
+        '''Test objects are saved to the db'''
+        types = TeamType.objects.all()
+
+        self.assertEqual(types.count(), 2)
+        self.assertEqual(types[0].type_name, 'One')
+        self.assertEqual(types[1].type_name, 'Two')
+
     def test_unicode_method(self):
         '''Ensure unicode method returns string of type_name'''
-        # pylint: disable=E1123, E1120
-        team_type = TeamType(type_abbr='NA', type_name='Name')
-        self.assertEqual(str(team_type), team_type.type_name)
+        the_type = TeamType.objects.first()
+        self.assertEqual(str(the_type), the_type.type_name)
