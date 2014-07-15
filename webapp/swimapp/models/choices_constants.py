4adbdb574e80e52afbdbc7bfd4048fe1e88202cd
Matthew Basanta <matthew.basanta@gmail.com>
2014-07-14 20:57:12 -0400

    Add file for model choices constants

---
+++ webapp/swimapp/models/choices_constants.py
@@ -0,0 +1,14 @@
+'''Constants for field lookup choices'''
+_YARDS = 'Y'
+_METERS = 'M'
+DISTANCE_UNIT_CHOICES = (
+    (_YARDS, 'Yards'),
+    (_METERS, 'Meters'),
+)
+
+_MALE = 'M'
+_FEMALE = 'F'
+GENDER_CHOICES = (
+    (_MALE, 'Male'),
+    (_FEMALE, 'Female'),
+)
