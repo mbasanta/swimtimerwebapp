c157838355593797687e05fbb93e0410d5c3f625
Matthew Basanta <matthew.basanta@gmail.com>
2014-08-10 19:58:58 -0400

    Set up logging

--- webapp/webapp/settings/base.py
+++ webapp/webapp/settings/base.py
@@ -294,13 +294,36 @@ LOG_LEVEL = logging.INFO
 HAS_SYSLOG = True
 SYSLOG_TAG = "http_app_webapp"  # Make this unique to your project.
 # Remove this configuration variable to use your custom logging configuration
-LOGGING_CONFIG = None
 LOGGING = {
     'version': 1,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] " +
+                      "%(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'django.log',
+            'formatter': 'verbose'
+        },
+    },
     'loggers': {
-        'webapp': {
-            'level': "DEBUG"
-        }
+        'django': {
+            'handlers': ['file'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+        'swimapp': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
     }
 }
 
