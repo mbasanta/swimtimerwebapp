743f28662645d0598917ca3afdf17727c1ebd8f1
Matthew Basanta <matthew.basanta@gmail.com>
2014-08-11 20:24:18 -0400

    Remove Heat objects, change heat to an integer value in entry. Fixes Issue #28

--- webapp/swimapp/admin.py
+++ webapp/swimapp/admin.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from .models import Team, TeamRegistration, TeamType
 from .models import Meet, MeetType, CourseCode
-from .models import Stroke, Event, Heat
+from .models import Stroke, Event
 from .models import MeetEvent
 from .models import MeetAdmin, EventAdmin
 from .models import MeetConfig
@@ -21,7 +21,6 @@ admin.site.register(MeetType)
 admin.site.register(CourseCode)
 admin.site.register(Stroke)
 admin.site.register(Event, EventAdmin)
-admin.site.register(Heat)
 admin.site.register(MeetEvent)
 admin.site.register(MeetConfig)
 admin.site.register(Version)
