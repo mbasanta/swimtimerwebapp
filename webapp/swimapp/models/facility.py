9693bdc22ff2a25cc331bfa4f326e78d871b025d
Matthew Basanta <matthew.basanta@gmail.com>
2014-09-02 19:48:16 -0400

    Update model and serializers for lane_count. Work on Issue #54

--- webapp/swimapp/models/facility.py
+++ webapp/swimapp/models/facility.py
@@ -28,7 +28,8 @@ class Facility(models.Model):
                                  related_name='length_2_set',
                                  blank=True,
                                  null=True)
-    lane_count = models.IntegerField(blank=True, null=True)
+    lane_count_1 = models.IntegerField(blank=True, null=True)
+    lane_count_2 = models.IntegerField(blank=True, null=True)
     addr_name = models.CharField(max_length=30, blank=True, null=True)
     addr = models.CharField(max_length=30, blank=True, null=True)
     addr_city = models.CharField(max_length=30, blank=True, null=True)
