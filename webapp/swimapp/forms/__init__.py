ebc0129e0dd186152c0c569a37ba99e934edd0c7
Matthew Basanta <matthew.basanta@gmail.com>
2014-09-07 20:24:11 -0400

    Remove old code from __init__.py

--- webapp/swimapp/forms/__init__.py
+++ webapp/swimapp/forms/__init__.py
@@ -1,8 +1,1 @@
-from django.forms import ModelForm
-from .models import Team
-
-
-class TeamForm(ModelForm):
-    class Meta:
-        model = Team
-        exclude = ["addr_county"]
+'''Forms for Swimapp'''
