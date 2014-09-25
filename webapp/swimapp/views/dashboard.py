ba578d8313904c0b205a3676bb80daf84cf8878b
Matthew Basanta <matthew.basanta@gmail.com>
2014-09-24 20:57:10 -0400

    Set up tabs on dashboard

--- webapp/swimapp/views/dashboard.py
+++ webapp/swimapp/views/dashboard.py
@@ -1,4 +1,5 @@
 '''Dashboard views for the swimapp'''
+import random
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
@@ -11,3 +12,8 @@ class DashboardView(TemplateView):
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(DashboardView, self).dispatch(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        context['random_number'] = random.randrange(1, 100)
+        return context
