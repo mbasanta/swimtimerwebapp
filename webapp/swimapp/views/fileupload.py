5bafa0a2284c7bc6d306aec69d9b8cd50ef1b914
Matthew Basanta <matthew.basanta@gmail.com>
2014-11-03 19:52:31 -0500

    Set up Ajax file uploads

--- webapp/swimapp/views/fileupload.py
+++ webapp/swimapp/views/fileupload.py
@@ -1,10 +1,15 @@
 '''Dashboard views for the swimapp'''
+import json
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse_lazy
 from django.utils.decorators import method_decorator
-from django.http import HttpResponseRedirect
-from django.views.generic import CreateView, UpdateView, DetailView, ListView
+from django.http import (HttpResponseRedirect, HttpResponse,
+                         HttpResponseBadRequest)
+from django.views.generic import UpdateView, ListView
 from django.views.generic import TemplateView
+from django.views.generic.base import View
+from django.views.generic.detail import SingleObjectTemplateResponseMixin
+from django.views.generic.edit import ModelFormMixin
 from swimapp.forms.fileupload import FileUploadForm
 from swimapp.models.fileupload import FileUpload
 from swimapp.tasks import process_hy3_upload
@@ -41,25 +46,44 @@ class FileUploadList(ListView):
         return super(FileUploadList, self).dispatch(*args, **kwargs)
 
 
-class FileUploadCreate(CreateView):
+class FileUploadCreate(SingleObjectTemplateResponseMixin,
+                       ModelFormMixin, View):
     '''File upload create view'''
     model = FileUpload
     template_name = 'swimapp/file_upload_edit.html'
     form_class = FileUploadForm
-    success_url = reverse_lazy('swimapp_file_upload_list')
+    #success_url = reverse_lazy('swimapp_file_upload_list')
 
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(FileUploadCreate, self).dispatch(*args, **kwargs)
 
-    def form_valid(self, form):
+    def form_valid(self, form, request):
         """
         If the form is valid, save the associated model.
         """
         self.object = form.save()
-        if (self.filetype == FileUpload.HY3_FILE):
+        if (self.object.filetype == FileUpload.HY3_FILE):
             process_hy3_upload.delay(self.object.id)
-        return HttpResponseRedirect(self.get_success_url())
+        if request.is_ajax():
+            return HttpResponse('OK')
+        else:
+            return HttpResponseRedirect(self.get_success_url())
+
+    def form_invalid(self, form, request):
+        """
+        If the form is invalid, re-render the context data with the
+        data-filled form and errors.
+        """
+        if request.is_ajax():
+            errors_dict = {}
+            if form.errors:
+                for error in form.errors:
+                    e = form.errors[error]
+                    errors_dict[error] = unicode(e)
+            return HttpResponseBadRequest(json.dumps(errors_dict))
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
 
     def post(self, request, *args, **kwargs):
         """
@@ -70,9 +94,17 @@ class FileUploadCreate(CreateView):
         form_class = self.get_form_class()
         form = self.get_form(form_class)
         if form.is_valid():
-            return self.form_valid(form)
+            return self.form_valid(form, request)
         else:
-            return self.form_invalid(form)
+            return self.form_invalid(form, request)
+
+    def put(self, *args, **kwargs):
+        return self.post(*args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        return self.render_to_response(self.get_context_data(form=form))
 
 
 class FileUploadUpdate(UpdateView):
